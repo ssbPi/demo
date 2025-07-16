e08b50b3412675911ca85d21535d4187f34f3bd5
ssbPi <raspissb@gmail.com>
2025-07-16 11:14:32 +0300

    Создание нового проекта
     Changes to be committed:
    	new file:   recrusivFunction/bin/Debug/recrusivFunction.exe
    	new file:   recrusivFunction/main.c
    	new file:   recrusivFunction/obj/Debug/main.o
    	new file:   recrusivFunction/recrusivFunction.cbp
    	new file:   recrusivFunction/recrusivFunction.depend

---
+++ recrusivFunction/main.c
@@ -0,0 +1,36 @@
+/*************************************************************************
+ Name:           SSB
+ E-mail:         mira_son@mail.ru
+ Date:          xx /xx  /2025
+ Version:        1.0
+*************************************************************************/
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int sum(int k);
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    int result = sum(10);
+    printf("%d", result);
+
+    return 0;
+}
+
+int sum(int k)
+{
+    if (k > 0)
+    {
+        return k + sum(k -1);
+    }
+    else
+    {
+        return 0;
+    }
+}
